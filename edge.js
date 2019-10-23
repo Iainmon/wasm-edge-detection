9ed28693978c8bb8f9287ac2f35f256018ce9d0e
Iain Moncrief <iainmoncrief@gmail.com>
2019-10-22 20:07:47 -1000

    edge and angle detection working

--- edge.js
+++ edge.js
@@ -1,14 +1,19 @@
+const ColorSpace = require('color-space');
 const Jimp = require('jimp');
 const KernelIterator = require('./KernelItorator');
 
 const greyscaleGrid = new Array();
 
-Jimp.read('./images/flower.jpeg', function (err, image) {
+console.log('Reading image...');
+
+Jimp.read('./images/flower2_exp.jpg', function (err, image) {
     if (err) { return console.error(err); }
 
+    // Gets image dimenstion
     const width = image.bitmap.width;
     const height = image.bitmap.height;
 
+    // Reads the image file into memory
     for (let x = 0; x < width; x++) {
 
         greyscaleGrid.push(new Array());
@@ -20,6 +25,36 @@ Jimp.read('./images/flower.jpeg', function (err, image) {
         }
     }
 
+    /*
+     *  Preprocesses image. (makes it blury)
+     */
+
+    console.log('Preprocessing image...');
+
+    let blurKernelMultipliers = [
+        [1, 2, 1],
+        [2, 4, 2],
+        [1, 2, 1]
+    ];
+
+    let blurKernelIterator = new KernelIterator(greyscaleGrid, blurKernelMultipliers);
+
+    let bluredGreyscaleGrid = blurKernelIterator.process();
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            let grey = Math.min(bluredGreyscaleGrid[x][y], 255);
+            greyscaleGrid[x][y] = grey;
+        }
+    }
+
+
+    /*
+     *  Edge detection
+     */
+
+    console.log('Calculating edges...');
+
     let kernelMultipliersX = [
         [-1, 0, 1],
         [-2, 0, 2],
@@ -27,13 +62,13 @@ Jimp.read('./images/flower.jpeg', function (err, image) {
     ];
     let kernelMultipliersY = [
         [-1, -2, -1],
-        [0,   0,  0],
-        [1,   2,  1]
+        [0, 0, 0],
+        [1, 2, 1]
     ];
 
     let kernelIteratorX = new KernelIterator(greyscaleGrid, kernelMultipliersX);
     let kernelIteratorY = new KernelIterator(greyscaleGrid, kernelMultipliersY);
-    
+
     let greyscaleX = kernelIteratorX.process();
     let greyscaleY = kernelIteratorY.process();
 
@@ -45,6 +80,7 @@ Jimp.read('./images/flower.jpeg', function (err, image) {
         }
     }
 
+    // Greyscale only
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             let grey = Math.min(newGreyScaleGrid[x][y], 255);
@@ -52,7 +88,40 @@ Jimp.read('./images/flower.jpeg', function (err, image) {
             image.setPixelColor(hex, x, y);
         }
     }
-    
+
+    /*
+     *  Color processing
+     */
+
+    console.log('Calculating edge orientations...');
+
+    let angleEdgeColors = new Array();
+
+    for (let x = 0; x < width; x++) {
+
+        angleEdgeColors.push(new Array());
+
+        for (let y = 0; y < height; y++) {
+
+            let orientation = Math.atan(greyscaleY[x][y] / greyscaleX[x][y]) * (180 / Math.PI);
+            orientation = isNaN(orientation) ? 0 : orientation;
+            let rgb = ColorSpace.hsl.rgb([orientation, 100, 50]);
+            angleEdgeColors[x][y] = { r: rgb[0] / 127, g: rgb[1] / 127, b: rgb[2] / 127 };
+
+        }
+    }
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            let grey = Math.min(newGreyScaleGrid[x][y], 255);
+            let color = angleEdgeColors[x][y];
+            let hex = Jimp.rgbaToInt(color.r * grey, color.g * grey, color.b * grey, 255);
+            image.setPixelColor(hex, x, y);
+        }
+    }
+
+
+    console.log('Saving image...');
     image.write('edge.jpeg');
 
 });
